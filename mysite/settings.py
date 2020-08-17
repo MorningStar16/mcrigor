6482820403770d50ebb2b28d4cef6f06f5b83660
morningstar16 <villaruel.mcrigor@gmail.com>
2020-08-17 21:03:04 +0800

    frm static to img hosting site

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+sTATIC_ROOT = (BASE_DIR / 'static')
 STATICFILES_DIRS = [BASE_DIR / "portfolio/static"]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (BASE_DIR / 'media')
